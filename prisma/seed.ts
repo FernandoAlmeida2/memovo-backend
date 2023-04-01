216b0571b51c0066edcf83f5c1b163f0e22552b6
Fernando Almeida <fernandoalmeida@fisica.ufc.br>
2023-04-01 20:03:22 -0300

    fix: changing database models and seed

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -3568,6 +3568,47 @@ async function main() {
       },
     ],
   });
+
+  await prisma.box.createMany({
+    data: [
+      {
+        name: "CURRENT"
+      },
+      {
+        name: "0-2-5-9"
+      },
+      {
+        name: "1-3-6-0"
+      },
+      {
+        name: "2-4-7-1"
+      },
+      {
+        name: "3-5-8-2"
+      },
+      {
+        name: "4-6-9-3"
+      },
+      {
+        name: "5-7-0-4"
+      },
+      {
+        name: "6-8-1-5"
+      },
+      {
+        name: "7-9-2-6"
+      },
+      {
+        name: "8-0-3-7"
+      },
+      {
+        name: "9-1-4-8"
+      },
+      {
+        name: "RETIRED"
+      },
+    ]
+  })
 }
 
 main()
